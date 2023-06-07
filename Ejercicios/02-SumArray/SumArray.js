c247dc5fedd3ac84cef84a24a4c8c3401f3edd10
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-06-07 13:55:19 -0500

    Resuelto el ejercicio SumArray

--- Ejercicios/02-SumArray/SumArray.js
+++ Ejercicios/02-SumArray/SumArray.js
@@ -1,6 +1,19 @@
 function SumArray (arr, n) {
-  // Your code here:
+  //[2, 4, 5, 9], 11
+  for (let i = 0; i < arr.length - 1; i++) {
 
+    for (let j = i + 1; j < arr.length; j++) {
+
+      if (arr[i] + arr[j] === n) {
+        return true; // Si encontramos dos números que suman el número dado, retornamos true
+      }
+    }
+  }
+  return false; // Si no se encontraron dos números que sumen el número dado, retornamos false
 }
+const arr = [2, 4, 5, 9]
+const num = 11
+
+console.log(`La suma de dos numeros cualesquiera en el array [${arr}] da como resultado ${num} ? ${SumArray(arr, num)}`);
 
 module.exports = SumArray
