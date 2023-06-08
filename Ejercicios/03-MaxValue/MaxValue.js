e30f1ea67cea4644b532024a2cef2fba820a3db5
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-06-08 09:58:48 -0500

    Resolvi el ejercicio 03-MaxValue y 04-MuldimensionalSumArray

--- Ejercicios/03-MaxValue/MaxValue.js
+++ Ejercicios/03-MaxValue/MaxValue.js
@@ -1,6 +1,29 @@
+/* Se nos presenta un arreglo de enteros, éstos representan el valor de una acción con el pasar del tiempo.
+ ¿El objetivo del ejercicio? Encontrar cuál es la máxima ganancia posible de comprar en un momento y 
+ vender en otro posterior. */
+//La compra debe ocurrir antes que la venta 
+
 function MaxValue (shares) {
   // Your code here:
+  //            =  4
+  let minPrecio = shares[0]; // Precio mínimo inicial
+  let maxGanancia = 0; // Ganancia máxima inicial
+  //       i= 4   i < 5     
+  for (let i = 1; i < shares.length; i++) {
+    //                 = 11
+    const precioActual = shares[i];
 
+    // Actualiza el precio mínimo si es menor que el actual
+    //        = 2
+    minPrecio = Math.min(minPrecio, precioActual); //Math.min(a, b) devuelve el valor mínimo entre a y b
+
+    // Calcula la ganancia utilizando Math.max
+    //          =  9         0, 11-2
+    maxGanancia = Math.max(maxGanancia, precioActual - minPrecio);
+  }
+
+  return maxGanancia;
 }
+console.log(MaxValue( [4, 3, 2, 5, 11] ));
 
 module.exports = MaxValue
--- Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,5 +1,29 @@
+/*
+El objetivo es encontrar la suma de todos los elementos del arreglo.
+
+Ejemplos
+mdArray: [1,2,3,4] | output: 10
+
+mdArraySum: [ [2,4] , [1], [4,2,1] ] | output: 14
+
+mdArraySum: [ 2, [3,4], 5, [-3, [6 , [ 4,5 ] ] ] ] | output: 26
+
+IMPORTANTE
+Para la resolución de este ejercicio no se puede utilizar el método Array.flat() */
+
 function mdArraySum (arr) {
   // Your code here:
+  let suma = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      suma += sumarArreglo(arr[i]);
+    } else {
+      suma += arr[i];
+    }
+  }
+  
+  return suma;
 
 }
 
